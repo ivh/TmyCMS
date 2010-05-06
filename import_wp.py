1e0deee3f7309ed011fded91a3ae49e82e353bce
Thomas Marquart <thomas@marquart.se>
2010-05-06 08:58:22 +0200

    get on to tags in import script

--- import_wp.py
+++ import_wp.py
@@ -39,7 +39,8 @@ for ID,post_date,post_title,post_content,post_name in posts:
     cursor.execute('select slug from wp_terms where wp_posts, wp_term_relationships, wp_term_taxonomy where wp_posts.ID="%s" AND wp_posts.ID=wp_term_relationships.object_ID AND wp_term_relationships.term_taxonomy_id=wp_term_taxonomy.term_taxonomy_id AND wp_term_taxonomy.term_id=wp_terms.term_id')
     tags=cursor.fetchall()
     for slug in tags:
-        #    entry.tags.add(Tag)
-        print post_name slug
+        tag=Tags.objects.get(slug=slug)
+        entry.tags.add(Tag)
+        print post_name,slug,Tag
 
     entry.save()
