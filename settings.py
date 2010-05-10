0949d5216f7ea665467080632dacdc04eb1af440
Thomas Marquart <thomas@marquart.se>
2010-05-10 14:46:10 +0200

    fixx settings, add tarski css files

--- settings.py
+++ settings.py
@@ -52,12 +52,12 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://all.tmy.se/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'd$@gw87j$(u#hfqm150*2rhkum1giv*zh2wp8lfn-6_0gfr97c'
