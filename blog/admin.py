07601036d61f5bda1cb501925e11fbf05db48e3e
Thomas Marquart <thomas@marquart.se>
2010-05-11 21:00:41 +0200

    work on search form

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from MyDjangoSites.blog.models import Tag,Entry
 from django.contrib import admin
 
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+    search_fields = ['title','body','slug']
+    date_hierarchy = 'pub_date'
+    list_filter = ['pub_date']
 
-admin.site.register(Entry)
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ['name']
+
+
+
+admin.site.register(Entry,EntryAdmin)
+admin.site.register(Tag,TagAdmin)
