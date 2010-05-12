fd43943d67f0bc2fd947f0ae773d4396e5b4055c
Thomas Marquart <thomas@marquart.se>
2010-05-12 13:01:06 +0200

    add textile to feed

--- blog/views.py
+++ blog/views.py
@@ -83,7 +83,7 @@ class LatestEntriesFeed(Feed):
         return item.title
 
     def item_description(self, item):
-        return textile.textile(item.body)
+        return textile.textile(item.body,encoding='utf8')
 
 
 def search(request):
