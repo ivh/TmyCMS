c029d44a2293f6ebe923af41ce84eeff7b360568
Thomas Marquart <thomas@marquart.se>
2010-05-13 08:37:40 +0200

    add autoescape off to tarski.html, also add admindocs in settings

--- settings.py
+++ settings.py
@@ -105,6 +105,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.markup',
     'django.contrib.flatpages',
     'django.contrib.comments',
